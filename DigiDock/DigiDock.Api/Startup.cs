5990601583f2428692c654ad3b64ee4ea2c14be2
Sabri Sinan Sağlam <152120181034@ogrenci.ogu.edu.tr>
2024-08-12 20:14:38 +0300

    SQL Connection Fix

--- DigiDock/DigiDock.Api/Startup.cs
+++ DigiDock/DigiDock.Api/Startup.cs
@@ -61,7 +61,7 @@ public class Startup
             .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
             .UseSimpleAssemblyNameTypeSerializer()
             .UseRecommendedSerializerSettings()
-            .UseSqlServerStorage(Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+            .UseSqlServerStorage(Configuration.GetConnectionString("HANGFIRE_CONNECTION"), new SqlServerStorageOptions
             {
                 CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
                 SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
